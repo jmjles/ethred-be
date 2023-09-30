22aa7bcc042e26ef9859ffdb01be70905d77276b
jmjles <jmjles@gmail.com>
2023-09-29 22:30:55 -0700

    devop: initial push

---
+++ index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response, Application } from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app: Application = express()
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Welcome to Express & TypeScript Server')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is Fire at http://localhost:${PORT}`)
+})
