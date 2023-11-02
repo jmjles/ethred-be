485eb375a0a96a0b959ebd814d44d508aeb8eb05
jmjles <jmjles@gmail.com>
2023-11-02 09:46:43 -0700

    devop: add types, add query logic

--- db/queries/userQueries.ts
+++ db/queries/userQueries.ts
@@ -31,22 +31,34 @@ export const getAllUsers = async () => {
     }
 }
 
-export const getFriends = async () => {
+export const getUsers = async (users: string[]) => {
     try {
+        return await model.find().all('id', users)
     } catch (er) {
         console.log(er)
         return []
     }
 }
-export const getBlockedUsers = async () => {
+
+export const getSuggestedUsers = async (
+    friends: string[],
+    blocked: string[]
+) => {
     try {
-    } catch (er) {
-        console.log(er)
-        return []
-    }
-}
-export const getSuggestedUsers = async () => {
-    try {
+        if (!friends.length) return []
+        const allUsers = await model.find().all('id', friends)
+        const cache: { [key: string]: any } = {}
+        const cacheBlocked: { [key: string]: any } = {}
+        blocked.forEach((b) => (cacheBlocked[b] = b))
+        const uniqueUsers: any[] = []
+        allUsers.forEach((user) => {
+            user.following.forEach((follower) => {
+                if (cache[follower.id] || cacheBlocked[follower.id]) return
+                cache[follower.id] = follower.id
+                uniqueUsers.push(follower)
+            })
+        })
+        return uniqueUsers
     } catch (er) {
         console.log(er)
         return []
@@ -54,14 +66,14 @@ export const getSuggestedUsers = async () => {
 }
 
 //* Update
-export const editUser = async () => {
+export const editUser = async (id:string,user:any) => {
     try {
     } catch (er) {
         console.log(er)
         return []
     }
 }
-export const banUser = async () => {
+export const banUser = async (id:string) => {
     try {
     } catch (er) {
         console.log(er)
@@ -70,14 +82,14 @@ export const banUser = async () => {
 }
 
 //* Delete
-export const deleteUser = async () => {
+export const deleteUser = async (id:string) => {
     try {
     } catch (er) {
         console.log(er)
         return []
     }
 }
-export const permaDeleteUser = async () => {
+export const permaDeleteUser = async (id:string) => {
     try {
     } catch (er) {
         console.log(er)
